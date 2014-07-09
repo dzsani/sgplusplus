fa1718a04779791b1b231634efdb04b5622c88c2
John Gera <gerajanos@gmail.com>
2014-07-09 19:15:00 +0200

    Added Firefox extension frame

---
+++ Firefox/lib/main.js
@@ -0,0 +1,9 @@
+var self = require('sdk/self');
+var pageMod = require("sdk/page-mod");
+
+pageMod.PageMod({
+	include: "*.sg.hu",
+	contentScriptWhen: "start",
+	attachTo: ["existing", "top"],
+	contentStyleFile: self.data.url('css/skin.css')
+});
