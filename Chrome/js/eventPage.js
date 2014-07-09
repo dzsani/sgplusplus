659ec61eab10a9516e987caaa1667fd37ae6fc89
John Gera <gerajanos@gmail.com>
2014-07-09 18:15:43 +0200

    Added Chrome extension frame

---
+++ Chrome/js/eventPage.js
@@ -0,0 +1,16 @@
+chrome.runtime.onInstalled.addListener(function() {
+
+});
+
+chrome.runtime.onSuspend.addListener(function() {
+
+});
+
+chrome.runtime.onSuspendCanceled.addListener(function() {
+
+});
+
+chrome.runtime.onMessage.addListener(function() {
+
+});
+
